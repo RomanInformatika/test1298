e88b0ee15d07a95c0b19b92dde8165e3f013a9c7
Лаврентьев Роман Владимирович <lavrentevrv@szao.obr.mos.ru>
2023-12-05 16:59:08 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,52 @@
+"use client"
 import Image from 'next/image'
-
+import { useState } from 'react'
+import { FaStarOfLife } from "react-icons/fa6";
 export default function Home() {
+  const [number, setNumber] = useState(0)
+  const increaseNumber = () => {
+
+
+  }
+
   return (
-    <p>Hello World</p>
+    <div className='flex flex-col bg-slate-600 w-full h-screen'>
+      <div className='bg-zinc-500 h-1/5'></div>
+      <div className='bg-neutral-300 h-full'>
+        <div className='flex flex-row h-20 justify-between'>
+          <button onClick={() => increaseNumber()}
+            className="flex flew-row justify-center items-center  px-5 min-w-[30%] py-3 m-2 text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 active:shadow-lg"
+          >
+  
+            <FaStarOfLife   />
+          
+          </button>
+          <button onClick={() => increaseNumber()}
+            className="px-2  min-w-[30%] py-2 m-2 text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 active:shadow-lg"
+          >
+            Button
+          </button>
+          <button onClick={() => increaseNumber()}
+            className="px-2 min-w-[30%] py-3 m-2 text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 active:shadow-lg"
+          >
+            Button
+          </button>
+
+        </div>
+
+
+
+
+      </div>
+
+    </div>
   )
 }
+
+
+
+{/* <button onClick={() => increaseNumber()}
+        className="px-5 py-3 text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 active:shadow-lg"
+      >
+        Button
+      </button> */}
